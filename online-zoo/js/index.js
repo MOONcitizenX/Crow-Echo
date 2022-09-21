34933fa29ff0c17ac6dd0ae6226726ebcda60f44
MOONcitizenX <mooncitizenx@gmail.com>
2022-09-21 16:40:11 +0300

    feat: add testimonials block

--- online-zoo/js/index.js
+++ online-zoo/js/index.js
@@ -1,5 +1,7 @@
 import Swiper from './swiper.js';
 
+const bamboo_hat = document.querySelector('.backstage_img');
+
 const petsSwiper = new Swiper('.cards_slider', {
 	slidesPerView: 3,
 	spaceBetween: 30,
@@ -9,3 +11,24 @@ const petsSwiper = new Swiper('.cards_slider', {
 		prevEl: '.swiper_button-prev'
 	}
 });
+
+const testimonialsSwiper = new Swiper('.testimonials_slider', {
+	slidesPerView: 4,
+	spaceBetween: 30,
+	scrollbar: {
+		el: '.swiper-scrollbar',
+		draggable: true,
+		snapOnRelease: true,
+		dragSize: 115
+	}
+});
+
+const updateOnResize = () => {
+	if (window.innerWidth > 1000) {
+		bamboo_hat.src = '../img/bamboo-cap.jpg';
+	} else if (window.innerWidth > 640) {
+		bamboo_hat.src = '../img/bamboo-cap_1000.jpg';
+	}
+};
+
+window.addEventListener('resize', updateOnResize);
