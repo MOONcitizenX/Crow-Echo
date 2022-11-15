809b7cc3c2b2c0cdb1535e354b7b61114800bcff
MOONcitizenX <mooncitizenx@gmail.com>
2022-11-15 19:47:26 +0300

    feat: add text

--- songbird/src/components/MainPage/Quiz/NextLvlBtn.js
+++ songbird/src/components/MainPage/Quiz/NextLvlBtn.js
@@ -9,22 +9,34 @@ export class NextLvlBtn extends BaseElement {
 			className: 'quiz__next-lvl',
 			attr: {
 				disabled: quizState.get().isAnswered ? true : false,
-				onclick: () => {
-					this.quizState.set({
-						quizLvl: this.quizState.get().quizLvl + 1,
-						dropStyles: true
-					});
-					this.quizState.set({
-						dropStyles: false,
-						isAnswered: false,
-						scoreLvl: 5,
-						quizCorrectAnswer: getRandomNum(0, 6)
-					});
+				onclick: (e) => {
+					const target = e.target;
+					if (target.id === 'winner') {
+						this.quizState.set({
+							success: true
+						});
+						window.location.href = '#results';
+					}
+					else {
+						this.quizState.set({
+							quizLvl: this.quizState.get().quizLvl + 1,
+							dropStyles: true
+						});
+						this.quizState.set({
+							dropStyles: false,
+							isAnswered: false,
+							scoreLvl: 5,
+							clickedCard: -1,
+							quizCorrectAnswer: getRandomNum(0, 6),
+							success: false
+						});
+					}
 				}
 			}
 		});
 		this.updateLang = (val) => {
-			this.elem.textContent = textContent[val].netx_btn;
+			this.elem.textContent =
+                textContent[val][this.quizState.get().quizLvl === 5 ? 'finish_game' : 'next_btn'];
 		};
 		this.updateState = (state) => {
 			const button = this.elem;
@@ -33,6 +45,14 @@ export class NextLvlBtn extends BaseElement {
 			}
 			else {
 				button.disabled = true;
+			}
+			this.elem.textContent =
+                textContent[this.lang.get()][state.quizLvl === 5 ? 'finish_game' : 'next_btn'];
+			if (state.quizLvl === 5) {
+				button.id = 'winner';
+			}
+			else {
+				button.id = '';
 			}
 		};
 		this.lang = langState;
--- songbird/src/textContent.js
+++ songbird/src/textContent.js
@@ -14,7 +14,16 @@ const textContent = {
 		],
 		error_message: 'Что-то пошло не так :( Пожалуйста, перейдите на другую страницу.',
 		score_msg: 'Счет: ',
-		netx_btn: 'Следующий вопрос'
+		default_msg: [
+			'Послушайте плеер.',
+			'Выберите подходящую птицу.',
+			'Нажмите "Следующий вопрос."'
+		],
+		next_btn: 'Следующий вопрос',
+		finish_game: 'Закончить игру',
+		congrats_msg: 'Поздравляем!',
+		new_game_btn: 'Начать новую игру',
+		results_error: 'Вы не закончили игру. Нажмите "Начать новую игру" чтобы сыграть!'
 	},
 	en: {
 		home: 'Home',
@@ -31,7 +40,16 @@ const textContent = {
 		],
 		error_message: 'Something went wrong ;( Please redirect to another page.',
 		score_msg: 'Score: ',
-		netx_btn: 'Next question'
+		default_msg: [
+			'Listen to player.',
+			'Choose the right bird.',
+			'Press "Next question".'
+		],
+		next_btn: 'Next question',
+		finish_game: 'Finish game',
+		congrats_msg: 'Congratulations!',
+		new_game_btn: 'Start new game',
+		results_error: 'You didn\'t finish the game. Press "Start new game" to play!'
 	}
 };
 export default textContent;
